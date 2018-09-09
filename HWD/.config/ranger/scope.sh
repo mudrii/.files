4c48d8ad2c2ff4930506722733482a04c2cc1df3
mudrii <mudreac@gmail.com>
2018-09-09 15:46:14 +0800

    add ranger video tambler

--- HWD/.config/ranger/scope.sh
+++ HWD/.config/ranger/scope.sh
@@ -109,10 +109,10 @@ handle_image() {
             exit 7;;
 
         # Video
-        # video/*)
+         video/*)
         #     # Thumbnail
-        #     ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
-        #     exit 1;;
+             ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
+             exit 1;;
         # PDF
          application/pdf)
              pdftoppm -f 1 -l 1 \

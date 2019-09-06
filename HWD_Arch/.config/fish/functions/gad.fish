f38a369b5929923a3f4b654f9ee26d93dc59cac4
mudrii <mudreac@gmail.com>
2019-09-06 20:54:57 +0800

    add less man function to HWD arch

---
+++ HWD_Arch/.config/fish/functions/gad.fish
@@ -0,0 +1,4 @@
+function gad --description 'alias git add'
+  git add . $argv;
+end
+
---
+++ HWD_Arch/.config/fish/functions/less.fish
@@ -0,0 +1,7 @@
+function less --description "wrap the 'less' page opener to use color in formatting"
+
+  set -gx LESSOPEN '|pygmentize -f terminal256 -g -P style=monokai %s'
+  set -gx LESS '-R'
+
+  command less  $argv
+end

dbbaf1e41015f304da5ded5a5daf6d8e69ef2f96
mudrii <mudreac@gmail.com>
2019-09-06 22:23:49 +0800

    optimize less and man

--- HWD_Arch/.config/fish/functions/less.fish
+++ HWD_Arch/.config/fish/functions/less.fish
@@ -3,5 +3,5 @@ function less --description "wrap the 'less' page opener to use color in formatt
   set -gx LESSOPEN '|pygmentize -f terminal256 -g -P style=monokai %s'
   set -gx LESS '-R'
 
-  command less  $argv
+  command less -m -g -i -J -u -Q $argv
 end
--- HWD_Arch/.config/fish/functions/man.fish
+++ HWD_Arch/.config/fish/functions/man.fish
@@ -1,9 +1,9 @@
 function man --description "wrap the 'man' manual page opener to use color in formatting"
   
   # start of bold:
-  set -x LESS_TERMCAP_md (set_color --bold red)
+  ##set -x LESS_TERMCAP_md (set_color --bold red)
   # end of all formatting:
-  set -x LESS_TERMCAP_me (set_color normal)
+  ##set -x LESS_TERMCAP_me (set_color normal)
 
   # start of standout (inverted colors):
   #set -x LESS_TERMCAP_so (set_color --reverse)
@@ -17,12 +17,12 @@ function man --description "wrap the 'man' manual page opener to use color in fo
   #set -x LESS_TERMCAP_ue (set_color normal)
   # (no change – I like the default)
 
-  #set -gx LESS_TERMCAP_md \e'[01;31m'
-  #set -gx LESS_TERMCAP_me \e'[0m'
-  #set -gx LESS_TERMCAP_se \e'[0m'
-  #set -gx LESS_TERMCAP_so \e'[01;44;33m'
-  #set -gx LESS_TERMCAP_ue \e'[0m'
-  #set -gx LESS_TERMCAP_us \e'[01;32m'
+  set -gx LESS_TERMCAP_md \e'[01;31m'
+  set -gx LESS_TERMCAP_me \e'[0m'
+  set -gx LESS_TERMCAP_se \e'[0m'
+  set -gx LESS_TERMCAP_so \e'[01;44;33m'
+  set -gx LESS_TERMCAP_ue \e'[0m'
+  set -gx LESS_TERMCAP_us \e'[01;32m'
 
   command man $argv
 end

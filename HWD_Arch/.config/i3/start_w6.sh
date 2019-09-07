d6bf43746a94741444dd980763fa05a9de78f8f1
mudrii <mudreac@gmail.com>
2019-09-07 18:14:11 +0800

    add cfg for mac and arch hwd

--- HWD_Arch/.config/i3/start_w6.sh
+++ HWD_Arch/.config/i3/start_w6.sh
@@ -5,6 +5,7 @@ i3-msg "workspace 6; append_layout ~/.config/i3/workspace_6.json"
 # And finally we fill the containers with the programs they had
 (zoom &)
 (slack &)
+(eddie-ui &)
 
 #i3-save-tree --workspace 6 > ~/.config/i3/workspace_6.json
 #tail -n +2 ~/.config/i3/workspace_6.json | fgrep -v '// splitv' | sed 's|//||g' > ~/.config/i3/workspace_6.1.json
---
+++ HWD_Mac/.config/fish/functions/cat.fish
@@ -0,0 +1,3 @@
+function cat --description 'alias cat pygmentize -f terminal256 -g -P style=monokai'
+  pygmentize -f terminal256 -g -P style=monokai $argv;
+end
